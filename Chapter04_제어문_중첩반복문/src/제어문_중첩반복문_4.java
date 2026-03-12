d20e8c2ec752fb83f96ea58ac80c541faa575714
sist <sist@sist-21>
2026-03-12 11:02:36 +0900

    1111

---
+++ Chapter04_제어문_중첩반복문/src/제어문_중첩반복문_4.java
@@ -0,0 +1,55 @@
+/*
+ *   ★★☆★★
+ *   ★★☆★★
+ *   ★★☆★★
+ *   ★★☆★★
+ *   ★★☆★★
+ *   
+ *   ☆★★★★
+ *   ★☆★★★
+ *   ★★☆★★
+ *   ★★★☆★
+ *   ★★★★☆
+ *   
+ *   ☆★★★☆
+ *   ★☆★☆★
+ *   ★★☆★★
+ *   ★☆★☆★
+ *   ☆★★★☆
+ *   
+ *   i  j
+ *   1  5
+ *   2  4
+ *   3  3
+ *   4  2
+ *   5  1   i+j=6  ==> j=6-i
+ */
+public class 제어문_중첩반복문_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        for(int i=1;i<=5;i++)
+        {
+        	for(int j=1;j<=5;j++)
+        	{
+        		//if(j==3)
+        		if(i==j || j==6-i)
+        		{
+        			System.out.print("☆");
+        		}
+        		else
+        		{
+        			System.out.print("★");
+        		}
+        	}
+        	System.out.println();
+        }
+        System.out.println("=========");
+        System.out.println("☆★★★☆");
+        System.out.println("★☆★☆★");
+        System.out.println("★★☆★★");
+        System.out.println("★☆★☆★");
+        System.out.println("☆★★★☆");
+	}
+
+}
