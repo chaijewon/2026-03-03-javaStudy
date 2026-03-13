f217c7e2ae10577d04f12a3c2b6ed3b88ae2568f
sist <sist@sist-21>
2026-03-13 15:21:44 +0900

    1111

--- Chapter05_데이터관리(배열)_2/src/배열_정리.java
+++ Chapter05_데이터관리(배열)_2/src/배열_정리.java
@@ -86,6 +86,61 @@ public class 배열_정리 {
             			+영화데이터.title[i]);
             	}
             }
+            else if(menu==2)
+            {
+            	System.out.print("상세볼 영화번호(1~1938):");
+            	int no=scan.nextInt();
+            	if(no<1 || no>1938)
+            	{
+            		System.out.println("없는 영화입니다!!");
+            		continue;
+            	}
+            	System.out.println("===== 영화 상세 =====");
+            	System.out.println("영화명:"+movie.title[no-1]);
+            	System.out.println("장르:"+movie.genre[no-1]);
+            	System.out.println("출연:"+movie.actor[no-1]);
+            	System.out.println("감독:"+movie.director[no-1]);
+            	System.out.println("개봉일:"+movie.regdate[no-1]);
+            	System.out.println("등급:"+movie.grade[no-1]);
+            }
+            else if(menu==3)
+            {
+            	System.out.print("검색어 입력:");
+            	String fd=scan.next();
+            	for(String data:movie.title)
+            	{
+            		if(data.contains(fd))
+            		{
+            			System.out.println(data);
+            		}
+            	}
+            }
+            else if(menu==4)
+            {
+            	System.out.print("배우명:");
+            	String fd=scan.next();
+            	for(int i=0;i<movie.actor.length;i++)
+            	{
+            		if(movie.actor[i].contains(fd))
+            		{
+            			System.out.println(movie.title[i]
+            					+"("+movie.actor[i]+")");
+            		}
+            	}
+            }
+            else if(menu==5)
+            {
+            	System.out.print("장르:");
+            	String fd=scan.next();
+            	for(int i=0;i<movie.genre.length;i++)
+            	{
+            		if(movie.genre[i].contains(fd))
+            		{
+            			System.out.println(movie.title[i]
+            					+"("+movie.genre[i]+")");
+            		}
+            	}
+            }
         }
 	}
 
