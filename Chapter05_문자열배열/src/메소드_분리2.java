089aa7e48e33dcf4c17a721671dbe0aa2e4cb4bf
sist <sist@sist-21>
2026-03-17 16:26:31 +0900

    1111

--- Chapter05_문자열배열/src/메소드_분리2.java
+++ Chapter05_문자열배열/src/메소드_분리2.java
@@ -8,6 +8,9 @@ import java.util.*;
  *        종료  
  *     2. 데이터 읽기 
  *     
+ *     객체 지향 
+ *     ------- Actor 
+ *     
  */
 public class 메소드_분리2 {
     
@@ -40,6 +43,35 @@ public class 메소드_분리2 {
 				for(int i=0;i<title.length;i++)
 				{
 					System.out.println((i+1)+"."+title[i]);
+				}
+			}
+			// 클래스 클래스 연결 => 메소드 
+			else if(m==2)
+			{
+				String[] title=GenieMusic.getSinger();
+				for(int i=0;i<title.length;i++)
+				{
+					System.out.println((i+1)+"."+title[i]);
+				}
+			}
+			else if(m==3)
+			{
+				String[] title=GenieMusic.getAlbum();
+				for(int i=0;i<title.length;i++)
+				{
+					System.out.println((i+1)+"."+title[i]);
+				}
+			}
+			else if(m==4)
+			{
+				System.out.print("검색어:");
+				String fd=scan.next();
+				String[] title=GenieMusic.getFind(fd);
+				System.out.println("검색 결과 "+title.length+"건");
+				for(int i=0;i<title.length;i++)
+				{
+					System.out.println((i+1)+"."+title[i]);
+					// 동영상 => youtube 
 				}
 			}
 		}
