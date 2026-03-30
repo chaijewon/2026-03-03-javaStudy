7478b9b6f0ca96ab0cae334aa7358599116e0ca8
sist <sist@sist-21>
2026-03-30 17:22:37 +0900

    1111

--- Chapter12_라이브러리(java.lang)/src/com/sist/customer/CustomerList.java
+++ Chapter12_라이브러리(java.lang)/src/com/sist/customer/CustomerList.java
@@ -19,7 +19,11 @@ implements ActionListener
     JTable table;
     DefaultTableModel model;
     JButton b1,b2;
-    
+    TableColumn column;
+    /// 데이터 제어 
+    int curpage=1;
+    int totalpage=0;
+    CustomerDataCollection cdc=new CustomerDataCollection();
     public CustomerList()
     {
     	la=new JLabel("사원 목록",JLabel.CENTER);
@@ -44,22 +48,86 @@ implements ActionListener
     	
     	table=new JTable(model);
     	JScrollPane js=new JScrollPane(table);
-    	
+    	table.setRowHeight(40);
+    	table.getTableHeader().setReorderingAllowed(false);
     	// 배치 
     	setLayout(null);
     	la.setFont(new Font("굴림체",Font.BOLD,50));
     	la.setBounds(10, 15, 920, 60);
     	add(la);
     	
+    	js.setBounds(10, 90, 920, 500);
+    	add(js);
     	
+    	JPanel p=new JPanel();
+    	p.add(b1);p.add(la1);p.add(b2);
     	
+    	p.setBounds(10, 600, 920, 35);
+    	add(p);
     	
+    	for(int i=0;i<col.length;i++)
+    	{
+    		column=table.getColumnModel().getColumn(i);
+    		if(i==0)
+    			column.setPreferredWidth(100);
+    		else if(i==1)
+    			column.setPreferredWidth(100);
+    		else if(i==2)
+    			column.setPreferredWidth(400);
+    		else if(i==3)
+    			column.setPreferredWidth(120);
+    		else if(i==4)
+    			column.setPreferredWidth(80);
+    		
+    	}
+    	print();
+    	b1.addActionListener(this);
+    	b2.addActionListener(this);
     }
-    
+    public void print()
+    {
+    	// 테이블 지우기 
+    	for(int i=model.getRowCount()-1;i>=0;i--)
+    	{
+    		model.removeRow(i);
+    	}
+    	
+    	CustomerVO[] cust=cdc.customerListData(curpage);
+    	totalpage=cdc.customerTotalPage();
+    	
+    	for(CustomerVO vo:cust)
+    	{
+    		String[] data= {
+    				vo.getLogin_id(),
+    				vo.getName(),
+    				vo.getEmail(),
+    				vo.getLoc(),
+    				vo.getGrade()
+    		};
+    		model.addRow(data);
+    	}
+    	
+    	la1.setText(curpage+" page/ "+totalpage+" pages");
+    }
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		if(e.getSource()==b1)
+		{
+			if(curpage>1)
+			{
+				curpage--;
+				print();
+			}
+		}
+		else if(e.getSource()==b2)
+		{
+			if(curpage<totalpage)
+			{
+				curpage++;
+				print();
+			}
+		}
 	}
 
 }
