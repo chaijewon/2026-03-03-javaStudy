9bd9211e007d08c62056af6c08e0695b0499a06f
chaijewon <vcandjava@nate.com>
2026-04-08 16:59:33 +0900

    1111

--- Chapter18_입출력(Buffered)/src/com/sist/application/BoardVO.java
+++ Chapter18_입출력(Buffered)/src/com/sist/application/BoardVO.java
@@ -12,4 +12,47 @@ public class BoardVO implements Serializable{
     private String pwd; // 삭제 , 수정 => 본인 여부 확인 
     private Date regdate; // 등록일 
     private int hit; // 조회수 
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+    
 }
--- Chapter18_입출력(Buffered)/src/com/sist/ui/BoardMain.java
+++ Chapter18_입출력(Buffered)/src/com/sist/ui/BoardMain.java
@@ -2,9 +2,14 @@ package com.sist.ui;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+
+import com.sist.application.BoardSystem;
 public class BoardMain extends JFrame 
 implements ActionListener
 {
+	private int curpage=1;
+	private int totalpage=0;
+	BoardSystem bs=new BoardSystem();
     // 레이아웃 
 	CardLayout card=new CardLayout();
 	BoardList bList=new BoardList();
@@ -20,6 +25,10 @@ implements ActionListener
 		// 이벤트 등록 
 		bList.inBtn.addActionListener(this);
 		bInsert.b2.addActionListener(this);
+	}
+	public void getBoardData()
+	{
+		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
